d30670d6611743e5a363324acc498a7e57524eaa
Morgan Allen <morganallen1999@gmail.com>
2025-04-05 21:25:59 -0600

    initial

---
+++ script.js
@@ -0,0 +1,14 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Document is ready!');
+    
+    // Your JavaScript code goes here
+    
+    // Example: Add an event listener to a button
+    // const button = document.querySelector('#myButton');
+    // if (button) {
+    //     button.addEventListener('click', function() {
+    //         alert('Button clicked!');
+    //     });
+    // }
+});
